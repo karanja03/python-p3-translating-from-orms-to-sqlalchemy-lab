471679751ae639ccb90f245242ca8faf66114d72
karanja03 <wambuik03@gmail.com>
2023-08-30 11:18:00 +0300

    Complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,7 @@
 from models import Dog
 
-def create_table(base):
-     base.metadata.create_all()
+def create_table(base, engine):
+     base.metadata.create_all(bind=engine)
 
 def save(session, dog):
     session.add(dog)
@@ -11,14 +11,14 @@ def get_all(session):
     return session.query(Dog).all()
 
 def find_by_name(session, name):
-    return session.query(Dog).filter_by(name=name).all()
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
         return session.query(Dog).filter_by(id=id).first()
 
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter_by(name=name, breed=breed).all()
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
     dog.breed = breed
